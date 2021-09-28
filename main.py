0d9bd6ee56f353a9e0d41fa263011d4bf24323db
julianjimenezm <79744990+julianjimenezm@users.noreply.github.com>
2021-09-28 17:21:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import re
 
 client = commands.Bot(command_prefix='$')
 
-1.# veryfing connection: ok
+#1. veryfing connection: ok
+
 @client.event
 async def on_ready():
     print('the bot is already ON ')
@@ -17,15 +18,17 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'ping back from {client.user} to {ctx.author} with latency equal to  {client.latency}')
 
-2# Deleting chat : ok
+#2. Deleting chat : ok
 
 @client.command()
 async def clear(ctx, amount = 5):
   await ctx.channel.purge(limit = amount)
   print(f'total messages deleted {amount}')
-@client.command()
+
 
 #3.First attempt - youtube connection
+
+@client.command()
 async def youtube(ctx, *, search):
     query = parse.urlencode({'search_query': search})
     html_content = request.urlopen('http://www.youtube.com/results?' + query)
